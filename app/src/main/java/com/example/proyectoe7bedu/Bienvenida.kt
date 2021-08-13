fd4f6398e2ee77f8fd41cd1545d9b35572d6ce99
alejandro lopez <alejandro.lopezgzz7@gmail.com>
2021-08-12 20:12:11 -0500

    Actualización de recycler view y LogIn ALopez

--- app/src/main/java/com/example/proyectoe7bedu/Bienvenida.kt
+++ app/src/main/java/com/example/proyectoe7bedu/Bienvenida.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.material.button.MaterialButton
 
 
 class Bienvenida : AppCompatActivity() {
@@ -12,17 +13,17 @@ class Bienvenida : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bienvenida)
 
-        val boton1=findViewById<Button>(R.id.buttonAccept)
+        val boton1=findViewById<MaterialButton>(R.id.buttonAccept)
         boton1.setOnClickListener {
             val intento1 = Intent(this, LogIn::class.java)
             startActivity(intento1)
         }
-        val boton2=findViewById<Button>(R.id.poli_priv)
+        val boton2=findViewById<MaterialButton>(R.id.poli_priv)
         boton2.setOnClickListener {
             val intento2 = Intent(this, Poli_Priva::class.java)
             startActivity(intento2)
         }
-        val boton3=findViewById<Button>(R.id.ter_ser)
+        val boton3=findViewById<MaterialButton>(R.id.ter_ser)
         boton3.setOnClickListener {
             val intento3 = Intent(this, Ter_Servicio::class.java)
             startActivity(intento3)
